fa9eed43cd94fc01165e02d012ccb8c55195246b
cornerStone-Dev <42221661+cornerStone-Dev@users.noreply.github.com>
2020-01-14 21:46:32 -0500

    fixed license comment

--- src/std_types.h
+++ src/std_types.h
@@ -1,12 +1,12 @@
 /* new standard types */
+/* florida_c compiler */
+/* PUBLIC DOMAIN */
 
 typedef uint8_t  u8;
 typedef int8_t   s8;
 typedef uint16_t u16;
 typedef int16_t  s16;
 typedef uint32_t u32;
-/* florida_c compiler */
-/* PUBLIC DOMAIN */
 typedef int32_t  s32;
 typedef uint64_t u64;
 typedef int64_t  s64;
